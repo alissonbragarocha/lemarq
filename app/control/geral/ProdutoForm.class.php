f5cf5fc21a729371d256bcd69b98bd1fe7b5f3d5
Alisson Braga da Rocha <alisson.braga.rocha@gmail.com>
2024-01-06 13:09:20 -0300

    Inclusão de histórico de alterações como uma datagrid

--- app/control/geral/ProdutoForm.class.php
+++ app/control/geral/ProdutoForm.class.php
@@ -6,6 +6,9 @@
 class ProdutoForm extends TPage
 {
     protected $form; // form
+    protected $datagrid;
+    protected $historico;
+
     private $database       = 'lemarq';
     private $activeRecord   = 'Produto';
     private $listView       = 'ProdutoList';
@@ -23,6 +26,7 @@ class ProdutoForm extends TPage
     public function __construct( $param, $embed = false )
     {
         parent::__construct();
+        parent::setProperty('override', 'true');
 
         $this->embed = $embed; // indica se o formulario está embutido noutra coisa.
                                      // Se estiver, alguns botoes e funcionalidades ficam limitados.
@@ -43,28 +47,53 @@ class ProdutoForm extends TPage
         $descricao = new TEntry('descricao');
         $valor_compra = new TNumeric('valor_compra', 2, ',', '.', true);
         $valor_venda = new TNumeric('valor_venda', 2, ',', '.', true);
+        $historico = new BPageContainer();
 
         // set sizes
         $id->setSize('100%');
         $descricao->setSize('100%');
         $valor_compra->setSize('100%');
         $valor_venda->setSize('100%');
+        $historico->setSize('100%');
 
         if (!empty($id))
         {
             $id->setEditable(FALSE);
         }
-        
+
+        $historico->setAction(new TAction(['ProdutoHistoricoList', 'onShow']));
+        $historico->setId('b645e36f5cc3c4');
+        $historico->hide();
+
         $descricao->addValidation( '<b>Descrição</b>', new TRequiredValidator );
         $valor_compra->addValidation( '<b>Valor de compra</b>', new TRequiredValidator );
         $valor_venda->addValidation( '<b>Valor de venda</b>', new TRequiredValidator );
+        
+        $loadingContainer = new TElement('div');
+        $loadingContainer->style = 'text-align:center; padding:50px';
+
+        $icon = new TElement('i');
+        $icon->class = 'fas fa-spinner fa-spin fa-3x';
+
+        $loadingContainer->add($icon);
+        $loadingContainer->add('<br>Carregando');
+
+        $historico->add($loadingContainer);
+
+        $this->historico = $historico;
+
+        $this->form->appendPage('Dados gerais');
 
         // add the fields
         $this->form->addFields( [ new TLabel('Id'), $id ] );
         $this->form->addFields( [ new TLabel('Descrição'), $descricao ] );
-        $row = $this->form->addFields( [ new TLabel('Valor de compra'), $valor_compra ], [ new TLabel('Valor de venda'), $valor_venda ] );
-        $row->layout = ['col-sm-6', 'col-sm-6'];
-         
+        $this->form->addFields( [ new TLabel('Valor de compra'), $valor_compra ] );
+        $this->form->addFields( [ new TLabel('Valor de venda'), $valor_venda ] );
+        
+        $this->form->appendPage("Históricos");
+        $row = $this->form->addFields([$historico]);
+        $row->layout = ['col-sm-12'];
+
         // create the form actions
         $btn = $this->form->addAction(_t('Save'), new TAction([$this, 'onSave']), 'fa:check');
         $btn->class = 'btn btn-sm btn-primary';
@@ -147,6 +176,10 @@ class ProdutoForm extends TPage
                 TTransaction::open($this->database); // open a transaction
                 $object = new ($this->activeRecord)($key); // instantiates the Active Record
                 $this->form->setData($object); // fill the form
+                
+                $this->historico->unhide();
+                $this->historico->setParameter('key', $object->{$this->keyField});
+
                 TTransaction::close(); // close the transaction
             }
             else
---
+++ app/control/geral/ProdutoHistoricoList.class.php
@@ -0,0 +1,155 @@
+<?php
+
+class ProdutoHistoricoList extends TPage
+{
+    private $datagrid;
+
+    use ListTrait;
+    
+    /**
+     * Class constructor
+     * Creates the page, the form and the listing
+     */
+    public function __construct($param)
+    {
+        parent::__construct();
+
+        $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
+        $this->datagrid->style = 'width: 100%';
+
+        $date       = new TDataGridColumn('logdate',    _t('Date'),   'center');
+        $system_user_id = new TDataGridColumn('system_user->name',      'Usuário',   'center');
+        $column     = new TDataGridColumn('columnname', _t('Column'), 'center');
+        $operation  = new TDataGridColumn('operation',  _t('Operation'), 'center');
+        $oldvalue   = new TDataGridColumn('oldvalue',   'Valor anterior', 'left');
+        $newvalue   = new TDataGridColumn('newvalue',   _t('New value'), 'left');
+        $access_ip  = new TDataGridColumn('access_ip',  'IP', 'center');
+        
+        $date->setTransformer( [$this, 'formatarDataHora'] );
+        
+        $operation->setTransformer( function($value, $object, $row) {
+            $div = new TElement('span');
+            $div->style="text-shadow:none; font-size:12px";
+            if ($value == 'created')
+            {
+                $div->class="label label-success";
+                $label = 'INSERIDO';
+            }
+            else if ($value == 'deleted')
+            {
+                $div->class="label label-danger";
+                $label = 'DELETADO';
+            }
+            else if ($value == 'changed')
+            {
+                $div->class="label label-info";
+                $label = 'ALTERADO';
+            }
+            $div->add($label);
+            return $div;
+        });
+        
+        $order = new TAction(array($this, 'onReload'), $param);        
+        $order->setParameter('order', 'logdate');
+        
+        $date->setAction($order);
+        
+        // adiciona as colunas à DataGrid
+        $this->datagrid->addColumn($date);
+        $this->datagrid->addColumn($system_user_id);
+        $this->datagrid->addColumn($column);
+        $this->datagrid->addColumn($operation);
+        $this->datagrid->addColumn($oldvalue);
+        $this->datagrid->addColumn($newvalue);
+        $this->datagrid->addColumn($access_ip);
+        
+        // cria o modelo da DataGrid, montando sua estrutura
+        $this->datagrid->createModel();
+        
+        // cria o paginador
+        $this->pageNavigation = new TPageNavigation;
+        $this->pageNavigation->setAction(new TAction(array($this, 'onReload'), $param));
+        $this->pageNavigation->setWidth($this->datagrid->getWidth());
+        
+        $panel = new TPanelGroup;
+        $panel->add($this->datagrid)->style='overflow-x:auto';
+        $panel->addFooter($this->pageNavigation);
+        
+        // vertical box container
+        $container = new TVBox;
+        $container->style = 'width: 100%';
+        $container->add($panel);
+        
+        parent::add($container);
+    }
+    
+    public function onReload($param = NULL)
+    {
+        try
+        {
+            // open a transaction with database
+            TTransaction::open('log');
+            
+            // creates a repository for Produto
+            $repository = new TRepository('SystemChangeLog');
+
+            $limit = 10;
+
+            // creates a criteria
+            $criteria = new TCriteria;
+            
+            // default order
+            if (empty($param['order']))
+            {
+                $param['order'] = 'id';
+                $param['direction'] = 'desc';
+            }
+            $criteria->setProperties($param); // order, offset
+            $criteria->setProperty('limit', $limit);
+
+            $criteria->add(new TFilter('tablename', '=', 'produto'));
+            $criteria->add(new TFilter('pkvalue', '=', $param['key']));
+            
+            // load the objects according to criteria
+            $objects = $repository->load($criteria, FALSE);
+            
+            if (is_callable($this->transformCallback))
+            {
+                call_user_func($this->transformCallback, $objects, $param);
+            }
+            
+            $this->datagrid->clear();
+            if ($objects)
+            {
+                // iterate the collection of active records
+                foreach ($objects as $object)
+                {
+                    // add the object inside the datagrid
+                    $this->datagrid->addItem($object);
+                }
+            }
+            
+            // reset the criteria for record count
+            $criteria->resetProperties();
+            $count = $repository->count($criteria);
+            
+            $this->pageNavigation->setCount($count); // count of records
+            $this->pageNavigation->setProperties($param); // order, page
+            $this->pageNavigation->setLimit($limit); // limit
+            
+            // close the transaction
+            TTransaction::close();
+            $this->loaded = true;
+        }
+        catch (Exception $e)
+        {
+            new TMessage('error', $e->getMessage());
+            TTransaction::rollback();
+        }
+    }
+
+    public function onShow($param = null)
+    {
+
+    }
+}
